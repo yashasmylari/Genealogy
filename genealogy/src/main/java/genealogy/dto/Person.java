596b9f64c62b53a74f9b84b6595e6a5ffbbfc9c7
YASHAS <yashasmylari93@gmail.com>
2019-05-08 20:09:55 +0200

    Removed the Person Attribute Label since all the properties are moved to the Person class Vivek Yadav

--- genealogy/src/main/java/genealogy/dto/Person.java
+++ genealogy/src/main/java/genealogy/dto/Person.java
@@ -16,7 +16,7 @@ import org.neo4j.ogm.annotation.Relationship;
 import genealogy.constants.Relations;
 
 @NodeEntity
-public class Person extends PersonAttributes {
+public class Person {
 
 	@Id
 	@GeneratedValue
@@ -24,6 +24,42 @@ public class Person extends PersonAttributes {
 
 	private String relation;
 	private String relationMap;
+
+	private String name;
+	private String gender;
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String placeOfBirth;
+	private Boolean isAlive;
+	private String dateOfDeath;
+	private String imageUrl;
+	private String imageData;
+	private String occupation;
+	private String education;
+	private String[] medicalCondition;
+
+
+	/* ***** SEPERATE ALL OF THE BELOW INTO DIFFERENT NODES ***** */
+
+	/*
+	String religion;
+	String clan;
+	String residence;
+	String nationality;
+	String ethnicity;
+
+	String[] locations;
+	String[] languages;
+	String[] physicalTraits;
+	String[] specialCharacteristic;
+
+	// New
+	String[] hobbies;
+	String[] cusine;
+	String[] favouriteGenre;
+	String[] favouriteSports;
+	*/
 
 	// Disabling it because it seems like an extra relationship that is not required
 	/*@Relationship(type = RELATED_TO)
@@ -221,4 +257,159 @@ public class Person extends PersonAttributes {
 		}
 		return jsonObject;
 	}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+
+	public Boolean getIsAlive() {
+		return isAlive;
+	}
+	public void setIsAlive(Boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+
+	public String getDateOfDeath() {
+		return dateOfDeath;
+	}
+	public void setDateOfDeath(String dateOfDeath) {
+		this.dateOfDeath = dateOfDeath;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public String getImageData() {
+		return imageData;
+	}
+	public void setImageData(String imageData) {
+		this.imageData = imageData;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+
+	public String[] getMedicalCondition() {
+		return medicalCondition;
+	}
+	public void setMedicalCondition(String[] medicalCondition) {
+		this.medicalCondition = medicalCondition;
+	}
+
+	/*
+	public String getResidence() {
+		return residence;
+	}
+	public void setResidence(String region) {
+		this.residence = region;
+	}
+
+	public String getReligion() {
+		return religion;
+	}
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+
+	public String getClan() {
+		return clan;
+	}
+	public void setClan(String clan) {
+		this.clan = clan;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String[] getPhysicalTraits() {
+		return physicalTraits;
+	}
+	public void setPhysicalTraits(String[] physicalTraits) {
+		this.physicalTraits = physicalTraits;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
+	}
+
+	public String getEthnicity() {
+		return ethnicity;
+	}
+	public void setEthnicity(String ethnicity) {
+		this.ethnicity = ethnicity;
+	}
+
+	public String[] getLocations() {
+		return locations;
+	}
+	public void setLocations(String[] locations) {
+		this.locations = locations;
+	}
+
+	public String[] getSpecialCharacteristic() {
+		return specialCharacteristic;
+	}
+	public void setSpecialCharacteristic(String[] specialCharacteristic) {
+		this.specialCharacteristic = specialCharacteristic;
+	}
+	*/
 }
